b5b10b0c03b58f30908b76928df6fd9be2870a2a
hansparrocho <s4176335@student.rmit.edu.au>
2026-06-02 21:48:02 +1000

    Added FuelType and LicenseType enums and compiled project successfully

---
+++ src/main/java/SRC/Main/FuelType.java
@@ -0,0 +1,18 @@
+package SRC.Main;
+
+/**
+ * FuelType represents the valid fuel types
+ * that a bus can use in the system.
+ *
+ * Buses may operate using:
+ * - Diesel
+ * - Hybrid
+ * - Electricity
+ */
+public enum FuelType {
+
+    DIESEL,
+    HYBRID,
+    ELECTRICITY
+
+}
---
+++ src/main/java/SRC/Main/LicenseType.java
@@ -0,0 +1,20 @@
+package SRC.Main;
+
+/**
+ * LicenseType represents the different driver
+ * licence categories supported by the system.
+ *
+ * Assignment Licence Types:
+ * - Light
+ * - Medium
+ * - Heavy
+ * - PublicTransport
+ */
+public enum LicenseType {
+
+    LIGHT,
+    MEDIUM,
+    HEAVY,
+    PUBLICTRANSPORT
+
+}
